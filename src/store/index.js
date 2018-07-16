c9cabd71c36d0341ff71463314a856c65d1b4579
yhf <absence.heart@qq.com>
2018-07-16 14:12:40 +0800

    Revert "ensure no error throwed in console"
    This reverts commit b73db2030369210e49ba9bdf25fa454674b0afa0.

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,25 @@ Vue.use(Vuex)
 console.log(Vuex.Store)
 export default new Vuex.Store({
   state: {
-    flag: false,
+    flag: true,
     goods: [],
     food: null,
     selectFood: [],
     totalCount: 0,
     totalPrice: 0
+  },
+  mutations: {
+    // selectFood () {
+    //   if (this.goods.length !== 0) {
+    //     this.goods.forEach(good => {
+    //       good.foods.forEach(food => {
+    //         if (food.count) {
+    //           foods.push(food)
+    //         }
+    //       })
+    //     })
+    //   }
+    //   return foods
+    // }
   }
 })
